137e378cab8ddd843aa2a45891ae27fa974dffc1
Grzegorz Nowak <strange3studio@gmail.com>
2018-08-23 13:11:21 +0200

    post standup #1

--- php/tests/ArrayTest.php
+++ php/tests/ArrayTest.php
@@ -5,13 +5,75 @@ use PHPUnit\Framework\TestCase;
 
 final class ArrayTest extends TestCase
 {
-    public function testAccessing()
-    {
-	$data = [1,2,3];
-        $this->assertEquals(
-            $data[0],
-            1
-        );
-    }
+	public function testAccessing()
+	{
+		$data = [1,2,3];
+		$this->assertEquals(
+			$data[0],
+			1
+		);
+	}
+
+	public function testSubselect()
+	{
+		$data = [1, 2, 3, 4];
+		$this->assertEquals(
+			array_slice($data, 1, 2),
+			[2, 3]
+		);
+	}
+
+	public function testMultiply()
+	{
+		$data = [1, 2, 3, 4];
+		$this->assertEquals(
+			array_map(function($elem) {
+				return $elem * 2;
+			}, $data),
+			[2, 4, 6, 8]
+		);
+		$this->assertEquals(
+			array_reduce($data, function($accum, $elem) {
+				if($elem % 2 == 0) {
+					$accum[] = $elem * 2;
+				}
+				return $accum;
+			}, []),
+			[4, 8]
+		);
+	}
+
+	public function testHeadTail() {
+		$data = [1, 2, 3, 4];
+		$head = $data[0];
+		$tail = array_slice($data, 1);
+
+		$this->assertEquals(
+			$head,
+			1
+		);
+
+		$this->assertEquals(
+			$tail,
+			[2, 3, 4]
+		);
+	}
+
+	public function testJoinArray() {
+		$data1 = [1, 2, 3, 4];
+		$data2 = ['a', 'b', 'c', 'd'];
+		$data3 = ['a', 'b', 'c'];
+
+		$this->assertEquals(array_map(function($element1, $element2) {
+			return [$element1, $element2];
+		}, $data1, $data2), [[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd']]);
+
+		$a = array_filter(array_map(function ($element) use ($data1, $data3) {
+			return isset($data3[array_search($element, $data1)]) ? [$element, $data3[array_search($element, $data1)]] : null;
+		}, $data1));
+		
+		$this->assertEquals($a, [[1, 'a'], [2, 'b'], [3, 'c']]);
+
+	}
 
 }
--- py/tests.py
+++ py/tests.py
@@ -5,6 +5,36 @@ class ArrayTests(unittest.TestCase):
         data = [1, 2, 3]
         self.assertEqual(data[0], 1)
 
+    def test_subselect(self):
+        data = [1, 2, 3, 4]
+        self.assertEqual(data[1:3], [2, 3])
+
+    def test_multiply(self):
+        data = [1, 2, 3, 4]
+        self.assertEqual([d * 2 for d in data], [2, 4, 6, 8])
+        self.assertEqual([d * 2 for d in data if d % 2 == 0], [4, 8])
+
+    def test_head_tail(self):
+        data = [1, 2, 3, 4]
+        head = data[0]
+        tail = data[1:]
+        self.assertEqual(head, 1)
+        self.assertEqual(tail, [2, 3, 4])
+
+    def test_join_array(self):
+        data1 = [1, 2, 3, 4]
+        data2 = ['a', 'b', 'c', 'd']
+        data3 = ['a', 'b', 'c']
+
+        self.assertEqual([[a, b] for a, b in zip(data1, data2)], [[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd']])
+        self.assertEqual([[a, b] for a, b in zip(data1, data3)], [[1, 'a'], [2, 'b'], [3, 'c']])
+
+
+class StringTests(unittest.TestCase):
+    def test_formatting(self):
+        template = "something {}. something {}"
+        self.assertEqual(template.format(1, 2), "something 1. something 2")
+
+
 if __name__ == "__main__":
     unittest.main()
-
